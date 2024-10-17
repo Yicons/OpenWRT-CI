72721f82a1432c3e34d07fcf12a781bb8b0b8447
Yicons <athony.tang@gmail.com>
2024-10-17 23:24:16 +0800

    1

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -1,35 +1,35 @@
-# #!/bin/bash
+#!/bin/bash
 
-# #安装和更新软件包
-# UPDATE_PACKAGE() {
-# 	local PKG_NAME=$1
-# 	local PKG_REPO=$2
-# 	local PKG_BRANCH=$3
-# 	local PKG_SPECIAL=$4
-# 	local REPO_NAME=$(echo $PKG_REPO | cut -d '/' -f 2)
+#安装和更新软件包
+UPDATE_PACKAGE() {
+	local PKG_NAME=$1
+	local PKG_REPO=$2
+	local PKG_BRANCH=$3
+	local PKG_SPECIAL=$4
+	local REPO_NAME=$(echo $PKG_REPO | cut -d '/' -f 2)
 
-# 	rm -rf $(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune)
+	rm -rf $(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune)
 
-# 	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
+	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
 
-# 	if [[ $PKG_SPECIAL == "pkg" ]]; then
-# 		cp -rf $(find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune) ./
-# 		rm -rf ./$REPO_NAME/
-# 	elif [[ $PKG_SPECIAL == "name" ]]; then
-# 		mv -f $REPO_NAME $PKG_NAME
-# 	fi
-# }
+	if [[ $PKG_SPECIAL == "pkg" ]]; then
+		cp -rf $(find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune) ./
+		rm -rf ./$REPO_NAME/
+	elif [[ $PKG_SPECIAL == "name" ]]; then
+		mv -f $REPO_NAME $PKG_NAME
+	fi
+}
 
-# #UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
+#UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 # UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
-# # UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
+# UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
 
-# # UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
-# # UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main"
-# # UPDATE_PACKAGE "nekoclash" "Thaolga/luci-app-nekoclash" "main"
-# # UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
-# UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
-# # UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
+# UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
+# UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main"
+# UPDATE_PACKAGE "nekoclash" "Thaolga/luci-app-nekoclash" "main"
+# UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
+UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
+# UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 
 # # UPDATE_PACKAGE "luci-app-advancedplus" "VIKINGYFY/luci-app-advancedplus" "main"
 # UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
